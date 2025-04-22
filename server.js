da5d0dd174dbe547aac5f6582ca6f3a8d12e3ddd
mdhawan3 <mihir.dhawan03@gmail.com>
2025-04-21 22:09:05 -0400

    hi
    hi

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // 👇 Replace this with YOUR secret key
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "AIzaSyCIujoslWN-H4qohlVd9xAnT0VhlcomqSk";
 
 app.use(express.static(__dirname)); // Serves index.html and assets
 app.use(express.json());
